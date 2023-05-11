779cc7d028f09b229372fd8a827994721d8b3b19
Shih-Yuan Lee (FourDollars) <fourdollars@debian.org>
2023-05-11 14:41:52 +0800

    Implement the GET method.

--- lp-api.go
+++ lp-api.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"github.com/pelletier/go-toml/v2"
 	"io/ioutil"
@@ -9,6 +10,8 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -21,7 +24,7 @@ type Credential struct {
 func request_token() (string, string) {
 	resp, err := http.PostForm("https://launchpad.net/+request-token",
 		url.Values{
-			"oauth_consumer_key":     {"System-wide: golang (lp-api)"},
+			"oauth_consumer_key":     {"System-wide: golang (https://github.com/fourdollars/lp-api)"},
 			"oauth_signature_method": {"PLAINTEXT"},
 			"oauth_signature":        {"&"},
 		},
@@ -35,7 +38,9 @@ func request_token() (string, string) {
 		panic(err)
 	}
 	mesg := string(body)
-	log.Print(mesg)
+	if *debug {
+		log.Print(mesg)
+	}
 	m, err := url.ParseQuery(mesg)
 	if err != nil {
 		panic(err)
@@ -49,7 +54,7 @@ again:
 	resp, err := http.PostForm("https://launchpad.net/+access-token",
 		url.Values{
 			"oauth_token":            {oauth_token},
-			"oauth_consumer_key":     {"System-wide: golang (lp-api)"},
+			"oauth_consumer_key":     {"System-wide: golang (https://github.com/fourdollars/lp-api)"},
 			"oauth_signature_method": {"PLAINTEXT"},
 			"oauth_signature":        {"&" + oauth_token_secret},
 		},
@@ -68,13 +73,15 @@ again:
 	} else if mesg == "End-user refused to authorize request token." {
 		return Credential{}, errors.New(mesg)
 	}
-	log.Print(mesg)
+	if *debug {
+		log.Print(mesg)
+	}
 	m, err := url.ParseQuery(mesg)
 	if err != nil {
 		return Credential{}, err
 	}
 	return Credential{
-		Key:    "System-wide: golang (lp-api)",
+		Key:    "System-wide: golang (https://github.com/fourdollars/lp-api)",
 		Token:  m["oauth_token"][0],
 		Secret: m["oauth_token_secret"][0],
 	}, nil
@@ -82,10 +89,10 @@ again:
 
 func get_credential() Credential {
 	var credential Credential
-	conf := os.Getenv("HOME") + "/.credential/lp-api.toml"
+	conf := os.Getenv("HOME") + "/.config/lp-api.toml"
 	if _, err := os.Stat(conf); os.IsNotExist(err) {
 		oauth_token, oauth_token_secret := request_token()
-		fmt.Printf("Please open https://launchpad.net/+authorize-token?oauth_token=%s&allow_permission=DESKTOP_INTEGRATION to authorize the token.\n", oauth_token)
+		log.Print(fmt.Sprintf("Please open https://launchpad.net/+authorize-token?oauth_token=%s&allow_permission=DESKTOP_INTEGRATION to authorize the token.", oauth_token))
 		credential, err = access_token(oauth_token, oauth_token_secret)
 		if err != nil {
 			panic(err)
@@ -113,21 +120,97 @@ func get_credential() Credential {
 		if err != nil {
 			panic(err)
 		}
-		log.Print("Found " + credential.Key + " " + credential.Token)
+		if *debug {
+			log.Print("Found " + credential.Key + " " + credential.Token)
+		}
 	}
 	return credential
 }
 
-func main() {
-	get_credential()
-	if len(os.Args) > 1 {
-		switch method := os.Args[1]; {
-		case method == "get":
-		case method == "download":
-		case method == "patch":
-		case method == "post":
-		default:
-			fmt.Printf("%s is not supported.\n", method)
+func set_auth_header(header *http.Header, credential Credential) {
+	var timestamp = time.Now().Unix()
+	var auth = fmt.Sprintf("OAuth realm=\"https://api.launchpad.net/\", oauth_consumer_key=\"%s\", oauth_token=\"%s\", oauth_signature=\"&%s\", oauth_nonce=\"%d\", oauth_signature_method=\"PLAINTEXT\", oauth_timestamp=\"%d\", oauth_version=\"1.0\"", credential.Key, credential.Token, credential.Secret, timestamp, timestamp)
+	header.Add("Authorization", auth)
+}
+
+func lp_get(resource string, args []string) (string, error) {
+	var credential = get_credential()
+	if *debug {
+		log.Print("GET ", resource, " ", args)
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.launchpad.net/devel/"+resource, nil)
+	set_auth_header(&req.Header, credential)
+	if len(args) > 0 {
+		q := req.URL.Query()
+		for _, arg := range args {
+			fields := strings.Split(arg, "=")
+			key := fields[0]
+			value := strings.Join(fields[1:], "=")
+			q.Add(key, value)
 		}
+		req.URL.RawQuery = q.Encode()
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	payload := string(body)
+	statusOK := resp.StatusCode >= 200 && resp.StatusCode < 300
+	if !statusOK {
+		var msg string
+		if strings.HasPrefix(payload, "Expired token") {
+			msg = payload + "\nPlease remove ~/.config/lp-api.toml to try iy again."
+		} else {
+			msg = strconv.Itoa(resp.StatusCode) + " " + http.StatusText(resp.StatusCode) + "\n" + payload
+		}
+		return payload, errors.New(msg)
+	}
+	return payload, nil
+}
+
+var debug = flag.Bool("debug", false, "Show debug messages")
+var help = flag.Bool("help", false, "Show help")
+
+func main() {
+	flag.Parse()
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
+	args := flag.Args()
+	if len(args) == 0 {
+		fmt.Println("Usage: lp-api {get,patch,put,post} resource, such as `lp-api get people/+me` or `lp-api get bugs/1`.\n\tPlease check https://api.launchpad.net/devel.html for details.")
+		flag.Usage()
+		os.Exit(0)
+	} else if len(args) == 1 {
+		fmt.Println("Usage: lp-api {get,patch,put,post} resource, such as `lp-api get people/+me` or `lp-api get bugs/1`.\n\tPlease check https://api.launchpad.net/devel.html for details.")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	switch method := args[0]; {
+	case method == "get":
+		payload, err := lp_get(args[1], args[2:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(payload)
+	case method == "patch":
+		fmt.Printf("%s is not implemented yet.\n", method)
+	case method == "put":
+		fmt.Printf("%s is not implemented yet.\n", method)
+	case method == "post":
+		fmt.Printf("%s is not implemented yet.\n", method)
+	case method == "download":
+		fmt.Printf("%s is not implemented yet.\n", method)
+	default:
+		fmt.Printf("%s is not supported.\n", method)
+		os.Exit(1)
 	}
 }
