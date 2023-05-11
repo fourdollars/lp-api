87cde6972719a0a502e78b57ac2ece29d3581808
Shih-Yuan Lee (FourDollars) <fourdollars@debian.org>
2023-05-11 10:49:45 +0800

    Save and read the token.

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module lp-api
 
 go 1.20
+
+require (
+	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
+)
--- lp-api.go
+++ lp-api.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/pelletier/go-toml/v2"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"time"
 )
+
+type Config struct {
+	Key    string `toml:"oauth_consumer_key"`
+	Token  string `toml:"oauth_token"`
+	Secret string `toml:"oauth_token_secret"`
+}
 
 func request_token() (string, string) {
 	resp, err := http.PostForm("https://launchpad.net/+request-token",
@@ -34,9 +43,9 @@ func request_token() (string, string) {
 	return m["oauth_token"][0], m["oauth_token_secret"][0]
 }
 
-func access_token(oauth_token string, oauth_token_secret string) (string, string) {
+func access_token(oauth_token string, oauth_token_secret string) (Config, error) {
 again:
-	time.Sleep(5 * time.Second)
+	time.Sleep(time.Second)
 	resp, err := http.PostForm("https://launchpad.net/+access-token",
 		url.Values{
 			"oauth_token":            {oauth_token},
@@ -46,29 +55,74 @@ again:
 		},
 	)
 	if err != nil {
-		panic(err)
+		return Config{}, err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return Config{}, err
 	}
 	mesg := string(body)
 	if mesg == "Request token has not yet been reviewed. Try again later." {
 		goto again
 	} else if mesg == "End-user refused to authorize request token." {
-		panic(mesg)
+		return Config{}, errors.New(mesg)
 	}
 	log.Print(mesg)
 	m, err := url.ParseQuery(mesg)
 	if err != nil {
-		panic(err)
+		return Config{}, err
 	}
-	return m["oauth_token"][0], m["oauth_token_secret"][0]
+	return Config{
+		Key:    "System-wide: golang (lp-api)",
+		Token:  m["oauth_token"][0],
+		Secret: m["oauth_token_secret"][0],
+	}, nil
 }
 
 func main() {
-	oauth_token, oauth_token_secret := request_token()
-	fmt.Printf("Please open https://launchpad.net/+authorize-token?oauth_token=%s&allow_permission=DESKTOP_INTEGRATION to authorize the token.\n", oauth_token)
-	access_token(oauth_token, oauth_token_secret)
+	conf := os.Getenv("HOME") + "/.config/lp-api.toml"
+	if _, err := os.Stat(conf); os.IsNotExist(err) {
+		oauth_token, oauth_token_secret := request_token()
+		fmt.Printf("Please open https://launchpad.net/+authorize-token?oauth_token=%s&allow_permission=DESKTOP_INTEGRATION to authorize the token.\n", oauth_token)
+		config, err := access_token(oauth_token, oauth_token_secret)
+		if err != nil {
+			panic(err)
+		}
+		bytes, err := toml.Marshal(&config)
+		if err != nil {
+			panic(err)
+		}
+		fp, err := os.Create(conf)
+		if err != nil {
+			panic(err)
+		}
+		defer fp.Close()
+		_, err = fp.Write(bytes)
+		if err != nil {
+			panic(err)
+		}
+		fp.Sync()
+	} else {
+		data, err := os.ReadFile(conf)
+		if err != nil {
+			panic(err)
+		}
+		var config Config
+		err = toml.Unmarshal([]byte(data), &config)
+		if err != nil {
+			panic(err)
+		}
+		log.Print("Found " + config.Key + " " + config.Token)
+	}
+	if len(os.Args) > 1 {
+		switch method := os.Args[1]; {
+		case method == "get":
+		case method == "download":
+		case method == "patch":
+		case method == "post":
+		default:
+			fmt.Printf("%s is not supported.\n", method)
+		}
+	}
 }
