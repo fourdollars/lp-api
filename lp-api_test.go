e3eeb4432199891bc837c8c40ac2def7b5a155a6
Shih-Yuan Lee (FourDollars) <fourdollars@debian.org>
2023-07-05 23:11:33 +0800

    Add some tests.

---
+++ lp-api_test.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func Test_get(t *testing.T) {
+	backupArgs := os.Args
+	os.Args = append(os.Args, "-staging")
+	os.Args = append(os.Args, "-output")
+	os.Args = append(os.Args, "payload.json")
+	os.Args = append(os.Args, "get")
+	os.Args = append(os.Args, "bugs/1923283")
+	main()
+	os.Args = backupArgs
+}
+
+func Test_put(t *testing.T) {
+	backupArgs := os.Args
+
+	os.Args = append(os.Args, "-staging")
+	os.Args = append(os.Args, "-output")
+	os.Args = append(os.Args, "")
+	os.Args = append(os.Args, "put")
+	os.Args = append(os.Args, "bugs/1923283")
+	os.Args = append(os.Args, "payload.json")
+	main()
+	os.Args = backupArgs
+}
+
+func Test_patch(t *testing.T) {
+	backupArgs := os.Args
+
+	os.Args = append(os.Args, "-staging")
+	os.Args = append(os.Args, "patch")
+	os.Args = append(os.Args, "bugs/1923283")
+	os.Args = append(os.Args, "tags:=[\"focal\",\"jammy\"]")
+	main()
+	os.Args = backupArgs
+
+	os.Args = append(os.Args, "-staging")
+	os.Args = append(os.Args, "patch")
+	os.Args = append(os.Args, "bugs/1923283")
+	os.Args = append(os.Args, "tags:=[]")
+	main()
+	os.Args = backupArgs
+}
+
+func Test_post(t *testing.T) {
+	backupArgs := os.Args
+
+	os.Args = append(os.Args, "-staging")
+	os.Args = append(os.Args, "post")
+	os.Args = append(os.Args, "bugs/1923283")
+	os.Args = append(os.Args, "ws.op=newMessage")
+	os.Args = append(os.Args, "content=test")
+	main()
+	os.Args = backupArgs
+}
+
+func Test_productionAPI(t *testing.T) {
+	os.Clearenv()
+	os.Setenv("LAUNCHPAD_TOKEN", "::")
+	backupArgs := os.Args
+
+	os.Args = append(os.Args, "-debug")
+	os.Args = append(os.Args, "get")
+	os.Args = append(os.Args, "https://api.launchpad.net/devel/bugs/1")
+	main()
+	os.Args = backupArgs
+}
+
+func Test_stagingAPI(t *testing.T) {
+	os.Clearenv()
+	os.Setenv("LAUNCHPAD_TOKEN", "::")
+	backupArgs := os.Args
+
+	os.Args = append(os.Args, "-debug")
+	os.Args = append(os.Args, "get")
+	os.Args = append(os.Args, "https://api.staging.launchpad.net/devel/bugs/1")
+	main()
+	os.Args = backupArgs
+}
+
+func Test_download(t *testing.T) {
+	os.Clearenv()
+	os.Setenv("LAUNCHPAD_TOKEN", "::")
+	backupArgs := os.Args
+
+	os.Args = append(os.Args, "-debug=0")
+	os.Args = append(os.Args, "download")
+	os.Args = append(os.Args, "https://api.launchpad.net/devel/bugs/1/+attachment/26604/data")
+	main()
+	os.Args = backupArgs
+}
